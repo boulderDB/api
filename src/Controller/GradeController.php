ceaaf2d0594a671f9561fc31a2194243134dbd7e
schaschjan <jan-jakub@atelierdisko.de>
2020-12-31 21:33:28 +0100

    wip

--- src/Controller/GradeController.php
+++ src/Controller/GradeController.php
@@ -12,8 +12,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  */
 class GradeController extends AbstractController
 {
-    private $entityManager;
-    private $contextService;
+    use ContextualizedControllerTrait;
+
+    private EntityManagerInterface $entityManager;
+    private ContextService $contextService;
 
     public function __construct(
         EntityManagerInterface $entityManager,
@@ -30,11 +32,15 @@ class GradeController extends AbstractController
     public function index()
     {
         $connection = $this->entityManager->getConnection();
-        $statement = 'select id, name, color from grade where tenant_id = :locationId';
-        $query = $connection->prepare($statement);
+        $statement = "select id, name, color from grade where tenant_id = :locationId and public = true";
 
+        if ($this->isLocationAdmin()) {
+            $statement = "select id, name, color from grade where tenant_id = :locationId";
+        }
+        
+        $query = $connection->prepare($statement);
         $query->execute([
-            'locationId' => $this->contextService->getLocation()->getId()
+            "locationId" => $this->contextService->getLocation()->getId()
         ]);
 
         $results = $query->fetchAll();
