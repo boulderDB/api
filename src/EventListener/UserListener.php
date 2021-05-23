097c7163d9c430c77a0a78a327435373162ac11b
schaschjan <jan-jakub@atelierdisko.de>
2021-05-23 23:09:11 +0200

    init notifications map through service

---
+++ src/EventListener/UserListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Service\NotificationService;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+class UserListener implements EventSubscriber
+{
+    private NotificationService $notificationService;
+
+    public function __construct(NotificationService $notificationService){
+
+        $this->notificationService = $notificationService;
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::prePersist,
+            Events::preUpdate
+        ];
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        /**
+         * @var \App\Entity\User $subject
+         */
+        $subject = $args->getObject();
+
+        $notifications = $this->notificationService->getNotificationsMap($subject);
+        $subject->setNotifications($notifications);
+    }
+}
