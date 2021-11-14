28e3e028172e4fed85be4de522f53255e7b69647
schaschjan <jan-jakub@atelierdisko.de>
2021-11-14 10:13:32 +0100

    wip

--- src/EventListener/AuthenticationSuccessSubscriber.php
+++ src/EventListener/AuthenticationSuccessSubscriber.php
@@ -2,73 +2,45 @@
 
 namespace App\EventListener;
 
-use App\Entity\Location;
 use App\Entity\User;
 use App\Repository\LocationRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AuthenticationSuccessSubscriber implements EventSubscriberInterface
 {
     private LocationRepository $locationRepository;
+    private Serializer $serializer;
 
     public function __construct(
         LocationRepository $locationRepository,
+        SerializerInterface $serializer
     )
     {
         $this->locationRepository = $locationRepository;
+        $this->serializer = $serializer;
     }
 
     public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): void
     {
-        $expiration = new \DateTime("+" . $_ENV["JWT_TOKEN_EXPIRATION"] . "seconds");
-
         /**
          * @var User $user
          */
         $user = $event->getUser();
-
         $payload = $event->getData();
+        $serializerGroups = ["groups" => "default"];
+
+        $lastVisitedLocation = $this->locationRepository->find($user->getLastVisitedLocation());
 
         $payload = array_merge($payload, [
-            "expiration" => $expiration->getTimestamp(),
             "target" => Request::createFromGlobals()->query->get("target"),
-            "targetLocation" => null,
             "location" => null,
-            "fullRegistration" => false,
-            "user" => [
-                "id" => $user->getId(),
-                "username" => $user->getUsername(),
-                "first_name" => $user->getFirstName(),
-                "last_name" => $user->getLastName(),
-                "roles" => $user->getRoles(),
-                "visible" => $user->isVisible(),
-            ]
+            "user" => $this->serializer->normalize($user, null, $serializerGroups),
+            "lastVisitedLocation" => $this->serializer->normalize($lastVisitedLocation, null, $serializerGroups)
         ]);
-
-        if ($user->getLastName() && $user->getLastName()) {
-            $payload["fullRegistration"] = true;
-        }
-
-        if ($user->getLastVisitedLocation()) {
-
-            /**
-             * @var Location $location
-             */
-            $location = $this->locationRepository->find($user->getLastVisitedLocation());
-
-            $payload["targetLocation"] = $location->getUrl();
-            $payload["location"] = [
-                "id" => $location->getId(),
-                "name" => $location->getName(),
-                "url" => $location->getUrl(),
-            ];
-        }
-
-        if ($_ENV["DEVELOPMENT_JWT_TOKEN"] === "true") {
-            $payload["developmentToken"] = $payload["token"];
-        }
 
         $event->setData($payload);
     }
