378e39903de1fbe19037f38f44aaccffff4228b8
schaschjan <jan-jakub@atelierdisko.de>
2021-12-07 22:30:18 +0100

    make event detail endpoint public

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -57,8 +57,6 @@ class EventController extends AbstractController
      */
     public function read(int $id)
     {
-        $this->denyUnlessLocationAdmin();
-
         return $this->readEntity(Event::class, $id, ["detail"]);
     }
 
