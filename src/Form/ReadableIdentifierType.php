c45028222615996dc9222c0d13efbd345c0f3274
schaschjan <jan-jakub@atelierdisko.de>
2022-01-20 09:42:14 +0100

    wip

--- src/Form/ReadableIdentifierType.php
+++ src/Form/ReadableIdentifierType.php
@@ -2,23 +2,27 @@
 
 namespace App\Form;
 
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class ReadableIdentifierType extends AbstractType
+class ReadableIdentifierType extends AbstractSchemaType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function getSchema(): array
     {
-        $builder->add("value", TextType::class, [
-            "constraints" => [
-                new NotBlank(),
-                new Length(["min" => 2, "max" => 50])
+        return [
+            [
+                "name" => "value",
+                "type" => TextType::class,
+                "options" => [
+                    "constraints" => [
+                        new NotBlank(),
+                        new Length(["min" => 2, "max" => 50])
+                    ]
+                ]
             ]
-        ]);
+        ];
     }
 
     public function configureOptions(OptionsResolver $resolver)
